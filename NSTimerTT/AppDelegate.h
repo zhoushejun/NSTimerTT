f2ab199982f0ac129269a1f912130cf40304dbb2
shejunzhou <965678322@qq.com>
2015-07-09 14:43:21 +0800

    Initial Commit

---
+++ NSTimerTT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSTimerTT
+//
+//  Created by shejun.zhou on 15/7/9.
+//  Copyright (c) 2015年 shejun.zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSTimerTT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSTimerTT
+//
+//  Created by shejun.zhou on 15/7/9.
+//  Copyright (c) 2015年 shejun.zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
