cb4cdac4bf352c8f2335d0d5cdfc405c6832b622
shejunzhou <965678322@qq.com>
2015-07-09 16:24:30 +0800

    save

--- NSTimerTT/AppDelegate.h
+++ NSTimerTT/AppDelegate.h
@@ -6,6 +6,12 @@
 //  Copyright (c) 2015年 shejun.zhou. All rights reserved.
 //
 
+/**
+ @file      AppDelegate.h
+ @abstract  后台运行定时器Demo
+ @author    shejun.zhou
+ @version   1.0 15/7/9 Creation
+ */
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
--- NSTimerTT/ViewController.m
+++ NSTimerTT/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
+NSString *str = @"已运行：";
+
 @interface ViewController ()
+
+@property (nonatomic, strong) NSTimer *myTimer;
+@property (weak, nonatomic) IBOutlet UILabel *labTime;
 
 @end
 
@@ -17,11 +22,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.labTime.text = [NSString stringWithFormat:@"%@0 s", str];
+    _myTimer = [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(loopPrintInfo:) userInfo:nil repeats:YES];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)loopPrintInfo:(id)info {
+    static NSInteger n = 0;
+    NSLog(@"%ld", ++n);
+    self.labTime.text = [NSString stringWithFormat:@"%@ %ld s", str, n];
 }
 
 @end
